b804f4d227288cc39f83868b3e87837c9d3aea01
Nicolas-Z-C <nicolascampus117@gmail.com>
2026-01-26 15:51:20 -0500

    Feat :sparkles: done whit the 1st exercise

---
+++ React-2/Curso-2/src/1er ejercicio/1ej.tsx
@@ -0,0 +1,21 @@
+import "../App.css";
+function Ejercicio1(){
+
+return(
+    <>
+        <div className="bg-amber-400 flex flex-col w-1/3 h-9/12 rounded-xl">
+            <img src="/Ejer1/asuka.jpg" alt="Feliz jueves" className="w-full h-9/12 rounded-xl"/>
+            <h1 className="self-center text-2xl">Asuka God</h1>
+        </div>
+        <div className="bg-amber-400 flex flex-col w-1/3 h-9/12 rounded-xl">
+            <img src="/Ejer1/Rei.jpg" alt="Rei" className="w-full h-9/12 rounded-xl"/>
+            <h1 className="self-center text-2xl">Rei</h1>
+        </div>
+        <div className="bg-amber-400 flex flex-col w-1/3 h-9/12 rounded-xl">
+            <img src="/Ejer1/mamisato.jpg" alt="mamisato" className="w-full h-9/12 rounded-xl"/>
+            <h1 className="self-center text-2xl">Mamisato</h1>
+        </div>
+    </>
+)}
+
+export default Ejercicio1   
---
+++ React-2/Curso-2/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Ejercicio1 from './1er ejercicio/1ej'
+
+function App() {
+
+  
+  return (
+    
+    <div className='flex flex-row w-full h-screen justify-evenly justify-self-center gap-4 pt-2 px-2'>
+    <Ejercicio1 />
+    </div>
+  )
+}
+
+export default App
